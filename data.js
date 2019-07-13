989331d697c463bc108f516302f57883165545c4
chamian10 <damianminniti@hotmail.com>
2019-07-13 16:30:44 -0300

    Add all

---
+++ data.js
@@ -0,0 +1,225 @@
+var data = {
+    "magazines": [
+        //COLLECTION 1
+        {
+            "title": "Ejemplar 1",
+            "regular_price": 150,
+            "promo_price": 75,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-549bde98-c579-4d49-9f59-279cae309642",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-fe590c6b-0913-4600-ad9d-2bc60a535b48",
+            "collection": "one",
+            // TODO REFACTOR
+            "discount": 0.5
+        },
+        {
+            "title": "Ejemplar 2",
+            "regular_price": 150,
+            "promo_price": 75,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-d9450ce0-ae71-4309-bc37-0e581b86c660",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-9383ec60-1e18-4430-9a14-74756958bf66",
+            "collection": "one"
+        },
+        {
+            "title": "Ejemplar 3",
+            "regular_price": 150,
+            "promo_price": 75,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-655756f0-d77d-4200-b327-08b49be59596",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-ec35173a-28cc-4de9-b2c0-c5dde7c69d66",
+            "collection": "one"
+        },
+        {
+            "title": "Ejemplar 4",
+            "regular_price": 150,
+            "promo_price": 75,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-1dd77709-5d57-4fd9-a8d9-28c517bc2b45",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-f0333df7-760b-4495-84f2-0e15b433ffe5",
+            "collection": "one"
+        },
+        {
+            "title": "Ejemplar 5",
+            "regular_price": 150,
+            "promo_price": 75,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-f2c76a51-3c65-44b5-9209-b2bc0d3103b3",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-6bf701f5-f113-42b2-b698-1983def16051",
+            "collection": "one"
+        },
+        {
+            "title": "Ejemplar 6",
+            "regular_price": 150,
+            "promo_price": 75,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-f037ec6d-3e76-408c-b3e8-dd310233b45f",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-ddb90ff8-f639-4c4b-adda-c176c0f36712",
+            "collection": "one"
+        },
+        {
+            "title": "Ejemplar 7",
+            "regular_price": 150,
+            "promo_price": 75,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-a9facfa4-9617-4784-9722-06538bb1cc3c",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-6aef77e6-dd51-44f4-9a8f-1a1425b1668d",
+            "collection": "one"
+        },
+        {
+            "title": "Ejemplar 8",
+            "regular_price": 150,
+            "promo_price": 75,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-1d18c242-8f9e-4fc8-ab33-69ff4d2ebdea",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-1c257f1b-4118-4f7e-9251-7cace27b6e02",
+            "collection": "one"
+        },
+        {
+            "title": "Ejemplar 9",
+            "regular_price": 150,
+            "promo_price": 75,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-2f497b9d-e8b1-49cc-85f8-222008b89d34",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-444665bd-b469-4249-9bf5-b28ebcd4db9b",
+            "collection": "one"
+        },
+        {
+            "title": "Ejemplar 10",
+            "regular_price": 150,
+            "promo_price": 75,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-2bac4969-1290-4489-8f71-a64aaff573fa",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-74360043-d53b-48fc-a5df-c3a1e768da75",
+            "collection": "one"
+        },
+        //COLLECTION 2
+        {
+            "title": "Ejemplar 1",
+            "regular_price": 150,
+            "promo_price": 75,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-58fa6e89-3412-432d-8785-c7f185738974",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-eda90e45-793e-40f4-8f1a-15e592b8362d",
+            "collection": "two"
+        },
+        {
+            "title": "Ejemplar 2",
+            "regular_price": 150,
+            "promo_price": 75,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-9d222aa5-a314-446d-a3ec-8d4dd8b7c67a",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-e5749000-4c72-44a5-8b4f-3e0454a8e281",
+            "collection": "two"
+        },
+        {
+            "title": "Ejemplar 3",
+            "regular_price": 150,
+            "promo_price": 75,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-39393c98-46f7-46cd-9fb4-ef8a3cc48ef3",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-4c799a4d-cf43-49a8-a546-2869f4d5f9f3",
+            "collection": "two"
+        },
+        {
+            "title": "Ejemplar 4",
+            "regular_price": 150,
+            "promo_price": 75,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-995029d1-52a8-4e09-addb-b7f27a394e95",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-c20f7077-495a-43b2-804c-dc7c31efd0f7",
+            "collection": "two"
+        },
+        {
+            "title": "Ejemplar 5",
+            "regular_price": 150,
+            "promo_price": 75,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-2aae1ad3-fe63-44bf-880d-6e79b5db4874",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-e5749000-4c72-44a5-8b4f-3e0454a8e281",
+            "collection": "two"
+        },
+        //SPECIALS
+        {
+            "title": "Especial fiestas saludables",
+            "regular_price": 200,
+            "promo_price": 100,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-2a35ff45-1090-40ec-ac57-35a21887de9c",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-7fc592c0-58d6-48c8-a34c-b66eb4b38e59",
+            "collection": "special"
+        },
+        {
+            "title": "Especial verano saludable",
+            "regular_price": 200,
+            "promo_price": 100,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-49350fbd-00e7-4318-a87a-dbf396d505d9",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-33f5271b-ac92-4b5a-a371-4edd3366df02",
+            "collection": "special"
+        },
+        {
+            "title": "Especial dulces",
+            "regular_price": 200,
+            "promo_price": 100,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-f4789f11-7431-42eb-92be-d2112f420121",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-b3c4403f-7a47-486b-88e7-fc04942a1621",
+            "collection": "special"
+        },
+        {
+            "title": "Recetas populares",
+            "regular_price": 250,
+            "promo_price": 125,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-1c45ef4b-99b2-4c8d-9320-719043f0958a",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-43b7f6a1-30d0-4409-90ed-985a2cbefee0",
+            "collection": "special"
+        },
+        //Colección 1 completa,
+        {
+            "title": "Colección 1 completa",
+            "regular_price": 1200,
+            "promo_price": 600,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-967be4b8-d503-4054-853c-e4f336a1cdf0",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-36d58eba-f197-4ddc-ad9f-e1236d4fa0c1",
+            "collection": "special"
+        },
+        //Colección 2 completa,
+        {
+            "title": "Colección 2 completa",
+            "regular_price": 600,
+            "promo_price": 300,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-24bd6e89-b047-4161-ad76-b6b5804ab2cb",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=192851636-b26939fd-f1bf-4413-a872-21eeba8ada51",
+            "collection": "special"
+        },
+        //Charla online,
+        {
+            "title": "Charla Eduacación alimentaria ",
+            "regular_price": 100,
+            "promo_price": 50,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-101cff52-20fd-4d1d-a109-ffa256858b28",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-57378221-6569-4af1-bbea-91c537b7f009",
+            "collection": "special"
+        },
+        //    EAN
+        {
+            "title": "Curso a distancia",
+            "regular_price": 1000,
+            "promo_price": 500,
+            "regular_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-2ef4e7ec-95e1-45b8-8a99-3546be436e74",
+            "promo_url": "https://www.mercadopago.com/mla/checkout/start?pref_id=239499703-06926a10-4885-430c-9aaa-9f9a27e78a7b",
+            "collection": "course"
+        }
+    ]
+}
+
+var output = document.getElementById('output');
+
+data.magazines.forEach(function (item, i) {
+
+    var magazineContainer = document.createElement('div');
+    magazineContainer.className = item.title;
+
+    var positionMagazine = document.createElement('div');
+    positionMagazine.className = "col-lg-2 col-md-4 col-sm-6 col-xs-6";
+
+    var wrapper = document.createElement('div');
+    wrapper.className = "wrapper";
+
+    var titleMagazine = document.createElement('div');
+    wrapper.innerHTML = item.title;
+
+    var buyBtn = document.createElement('a');
+    buyBtn.className = 'btn btn-primary background-btn-pink';
+    buyBtn.href= item.regular_url;
+    buyBtn.innerHTML = 'COMPRAR';
+
+    output.appendChild(magazineContainer);
+    magazineContainer.appendChild(positionMagazine);
+    positionMagazine.appendChild(wrapper);
+    wrapper.appendChild(titleMagazine);
+    wrapper.appendChild(buyBtn);
+})
